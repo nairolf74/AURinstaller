3851c8ccda09444b6e572e8a5a94d214b51c866e
florian <deschenf@gmail.com>
2020-11-20 15:37:20 +0100

    affichage version

--- src/AUR.c
+++ src/AUR.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#define VERSION "2.0.0"
+
 void help()
 {
-	fprintf(stdout,"[] -> aide\n");
+	fprintf(stdout,"[-h] -> aide\n");
 	fprintf(stdout,"[] -> fonctionnement sans passer par 'AUR'\n");
 	fprintf(stdout,"[] -> vide le cache\n");
 	fprintf(stdout,"[] -> liste les paquets installés depuis AUR\n");
 	fprintf(stdout,"[] -> liste les paquets non installés depuis pacman\n");
 	fprintf(stdout,"[] -> liste les paquets pouvant être mis à jour\n");
 	fprintf(stdout,"[] -> met à jour les paquets\n");
-	fprintf(stdout,"[] -> version du programme\n");
+	fprintf(stdout,"[-v] -> version du programme\n");
 	fprintf(stdout,"AUR [option 1] [nom paquet]\n");
 	fprintf(stdout,"[] -> clonage, création et installation du paquet\n");
 	fprintf(stdout,"[] -> clonage du paquet\n");
@@ -19,13 +21,18 @@ void help()
 	fprintf(stdout,"[] -> fini l'installation avec pacman");
 }
 
+void version()
+{
+	fprintf(stdout,"%s\n",VERSION);
+}
 
 int main(int argc,char * argv[])
 {
 if(argc>1)
 {
-	if(strcmp(argv[1],"--")==0)
+	if(strcmp(argv[1],"-v")==0)
 	{
+		version();
 	}
 	else
 	{
