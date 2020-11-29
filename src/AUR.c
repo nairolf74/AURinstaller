03301019308180d7c8b61382085b2841fc939083
florian <florian.deschenaux@etu.univ-smb.fr>
2020-11-29 15:46:58 +0100

    lister paquets non installés depuis pacman

--- src/AUR.c
+++ src/AUR.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 
 #define VERSION "2.0.0"
 
@@ -9,10 +12,10 @@ void help()
 	fprintf(stdout,"[-h] -> aide\n");
 	fprintf(stdout,"[] -> vide le cache\n");
 	fprintf(stdout,"[] -> liste les paquets installés depuis AUR\n");
-	fprintf(stdout,"[] -> liste les paquets non installés depuis pacman\n");
+	fprintf(stdout,"[-Qm] -> List installed packages not found in pacman\n");
 	fprintf(stdout,"[] -> liste les paquets pouvant être mis à jour\n");
 	fprintf(stdout,"[] -> met à jour les paquets\n");
-	fprintf(stdout,"[-v] -> version du programme\n");
+	fprintf(stdout,"[-V] -> version\n");
 	fprintf(stdout,"AUR [option 1] [nom paquet]\n");
 	fprintf(stdout,"[] -> clonage, création et installation du paquet\n");
 	fprintf(stdout,"[] -> clonage du paquet\n");
@@ -25,22 +28,36 @@ void version()
 	fprintf(stdout,"%s\n",VERSION);
 }
 
+void noFromPacman()
+{
+	int p=fork();
+	if(p==0)
+	{
+		execl("/bin/pacman","/bin/pacman","-Qm",(char*)NULL);
+	}
+}
+
 int main(int argc,char * argv[])
 {
-if(argc>1)
-{
-	if(strcmp(argv[1],"-v")==0)
-	{
-		version();
+	if(argc>1)
+ 	{
+		if(strcmp(argv[1],"-V")==0)
+		{
+			version();
+	 	}
+		else if(strcmp(argv[1],"-Qm")==0)
+		{
+			noFromPacman();
+		}
+		else
+	 	{
+			help();
+		}
 	}
 	else
 	{
 		help();
 	}
-	}
-	else
-	{
-		help();
-	}
+	wait(NULL);
 	return EXIT_SUCCESS;
 }
